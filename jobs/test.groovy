059983637fe7341dad4d73cc10d5bf123cbbd69b
Keto D. Zhang <keto.zhang@gmail.com>
2025-08-27 16:42:29 -0700

    Init starter helmfile, seed file, and jenkinsfile

---
+++ jobs/test.groovy
@@ -0,0 +1,33 @@
+#!/usr/bin/env groovy
+
+pipelineJob('test') {
+    logRotator {
+        numToKeep(10)
+        daysToKeep(30)
+    }
+
+    configure { project ->
+        project / 'properties' / 'org.jenkinsci.plugins.workflow.job.properties.DurabilityHintJobProperty' {
+            hint('PERFORMANCE_OPTIMIZED')
+        }
+    }
+
+    definition {
+        cpsScm {
+            scm {
+                git {
+                    remote {
+                        url('https://github.com/ketozhang/jenkins-in-k8-quickstart')
+                    }
+                    branches('*/main')
+                    extensions (
+                        cloneOptions {
+                            shallow(true)
+                        }
+                    )
+                }
+            }
+            scriptPath('jenkins/hello-world.jenkinsfile')
+        }
+    }
+}
